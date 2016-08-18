9327a3ed99e20129f83694ed1875a817c31dcca6
FACCI <FACCI@PC-06>
2016-08-19 01:40:39 +0200

    error

--- Cliente2/src/Main.java
+++ Cliente2/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        asdgdfhjfghkfgk
         // TODO code application logic here
     }
     
